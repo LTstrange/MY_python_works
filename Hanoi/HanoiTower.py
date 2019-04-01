69d3f7e153c9aa75e36e00cc3c232bd235a759ee
LTstrange <835422774@qq.com>
2019-04-01 21:00:36 +0800

    modify hanoi

--- Hanoi/HanoiTower.py
+++ Hanoi/HanoiTower.py
@@ -17,14 +17,14 @@ if __name__ == "__main__":
 
     var = tk.StringVar()
 
-    l = tk.Label(window, textvariable=var, bg='green', font=('Arial', 12), width=15, height=2)
+    l = tk.Label(window, textvariable=var, bg='white', font=('Arial', 12), width=15, height=2)
 
     l.pack()
     on_hit = False
 
     def hit_me():
         global on_hit
-        if on_hit == False:
+        if not on_hit:
             on_hit = True
             var.set('you hit me')
         else:
