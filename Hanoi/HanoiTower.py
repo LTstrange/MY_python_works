f4953ed6363636ca2ca34d7618fd110d3dc904f2
LTstrange <835422774@qq.com>
2019-04-02 20:53:08 +0800

    完全版

--- Hanoi/HanoiTower.py
+++ Hanoi/HanoiTower.py
@@ -1,31 +1,66 @@
-import tkinter as tk
+import turtle
+
+TURTLE_SPEED = 5
+
+def draw_poles(n):
+    t = turtle.Turtle()
+    t.ht()
+    t.pu()
+    t.speed(0)
+    t.pensize(5)
+    for i in range(3):
+        t.goto(-200+i*200, 10*n)
+        t.pd()
+        t.goto(-200+i*200, -100)
+        t.goto(-250+i*200, -100)
+        t.goto(-150+i*200, -100)
+        t.pu()
+    p_sq = dict(enumerate([[] for _ in range(3)]))
+    return p_sq
 
 
-def HanoiTowerMove(n, a, b, c):
+def set_blocks(n):
+    sq = []
+    for i in range(n):
+        t = turtle.Turtle()
+        t.speed(TURTLE_SPEED)
+        t.pu()
+        t.shape("square")
+        t.shapesize(1, i+1)
+        t.goto(-200, -110+20*(n-i))
+        sq.append(t)
+    return sq
+
+
+def move_blocks(blocks, poles, x, y):
+    i = poles[x/200+1][-1]
+    blocks[i].sety(150)
+    blocks[i].setx(y)
+    blocks[i].sety(-90+20*len(poles[y/200+1]))
+    poles[y/200+1].append(poles[x/200+1].pop())
+    print(poles)
+
+
+def hanoi_tower(b_sq, p_sq, n, a, b, c):
     if n == 1:
-        print(a, c)
+        move_blocks(b_sq, p_sq, a, c)
     else:
-        HanoiTowerMove(n-1, a, c, b)
-        HanoiTowerMove(1, a, b, c)
-        HanoiTowerMove(n-1, b, a, c)
+        hanoi_tower(b_sq, p_sq, n-1, a, c, b)
+        hanoi_tower(b_sq, p_sq, 1, a, b, c)
+        hanoi_tower(b_sq, p_sq, n-1, b, a, c)
 
 
 if __name__ == "__main__":
-    window = tk.Tk()
-    window.title("hanoi tower")
-    window.geometry('1200x800')
+    n = int(input("请输入汉诺塔层数并回车:"))
+    screen = turtle.Screen()
+    screen.screensize(canvwidth=600, canvheight=250)
+    screen.screensize(bg='gray')
+    p_sq = draw_poles(n)
+    l = [i for i in range(n)]
+    l.reverse()
+    p_sq[0].extend(l)
+    print(p_sq)
+    b_sq = set_blocks(n)  # sq按从小到大的顺序存放blocks
+    hanoi_tower(b_sq, p_sq, n, -200, 0, 200)
+    screen.exitonclick()
 
-    canvas = tk.Canvas(window, bg="white", height=400, width=800)
-
-    x0, y0, x1, y1 = 0, 0, 80, 80
-    oval = canvas.create_oval(x0, y0, x1, y1, anchor='center', fill='green')
-    rect = canvas.create_rectangle(0, 0, 50, 50, fill='blue')
-    canvas.pack()
-
-    def moveit():
-
-
-    b = tk.Button(window, text='move', command=moveit).pack()
-
-    window.mainloop()
-    # HanoiTowerMove(3, 'A', 'B', 'C')
